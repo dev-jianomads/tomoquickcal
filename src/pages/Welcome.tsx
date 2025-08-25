999fc63960d03d9cd5a0ba742b40a0ca67849e13
dev-jianomads <dev@jia-nomads.com>
2025-08-25 12:51:11 +1000

    Add debug UI display for Telegram detection

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -310,6 +310,16 @@ const Welcome: React.FC = () => {
         )}
       </div>
     </PageContainer>
+
+    {/* Debug UI - Remove this after testing */}
+    <div className="fixed bottom-4 right-4 bg-black bg-opacity-75 text-white text-xs p-3 rounded-lg max-w-xs">
+      <div className="font-bold mb-2">Debug Info:</div>
+      <div>isTelegramBrowser: {isTelegramBrowser.toString()}</div>
+      <div>isTgMiniApp: {isTgMiniApp.toString()}</div>
+      <div>isTelegramUA: {isTelegramUA.toString()}</div>
+      <div>referrer: {document.referrer || 'none'}</div>
+      <div>userAgent: {navigator.userAgent.substring(0, 50)}...</div>
+    </div>
   );
 };
 
